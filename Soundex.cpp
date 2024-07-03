19b37615f04bebcf153bde3913005300d3a6677d
Deepa-Dhanashekar <88785401+Deepa-Dhanashekar@users.noreply.github.com>
2024-07-03 15:07:03 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -10,14 +10,15 @@ char getSoundexCode(char c) {
         case 'L': return '4';
         case 'M': case 'N': return '5';
         case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
+        default: return '0'; // For vowels and other characters
     }
 }
 
 std::string generateSoundex(const std::string& name) {
     if (name.empty()) return "";
 
-    std::string soundex(1, toupper(name[0]));
+    std::string soundex;
+    soundex += toupper(name[0]);
     char prevCode = getSoundexCode(name[0]);
 
     for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
@@ -28,9 +29,7 @@ std::string generateSoundex(const std::string& name) {
         }
     }
 
-    while (soundex.length() < 4) {
-        soundex += '0';
-    }
+    soundex.append(4 - soundex.length(), '0');
 
     return soundex;
 }
